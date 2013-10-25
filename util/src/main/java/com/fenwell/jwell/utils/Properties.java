fa516a828f14f3984871da34cee86a04b2803c98
wang22 <pause22@163.com>
2013-10-25 09:33:08 +0800

    暂时删除Properties读取文件工具类代码

--- util/src/main/java/com/fenwell/jwell/utils/Properties.java
+++ util/src/main/java/com/fenwell/jwell/utils/Properties.java
@@ -2,22 +2,4 @@ package com.fenwell.jwell.utils;
 
 public class Properties {
 
-    private String file;
-
-    private Properties(String file) {
-        this.file = file;
-    }
-
-    public static Properties readProperties(String file) {
-        return new Properties(file);
-    }
-
-    public String get(String key) {
-        return null;
-    }
-
-    public String getDef(String key, String def) {
-        return null;
-    }
-
 }
