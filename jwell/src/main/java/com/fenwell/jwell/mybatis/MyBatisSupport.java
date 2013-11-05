ee8e9ce112614273079e6fd0587c5520ae0e8857
wang22 <pause22@163.com>
2013-11-05 15:59:13 +0800

    mybatis support

--- jwell/src/main/java/com/fenwell/jwell/mybatis/MyBatisSupport.java
+++ jwell/src/main/java/com/fenwell/jwell/mybatis/MyBatisSupport.java
@@ -1,10 +1,12 @@
 package com.fenwell.jwell.mybatis;
 
-import com.fenwell.jwell.mybatis.proxy.MyBatisProxy;
-
 import net.sf.cglib.proxy.Enhancer;
 
+import com.fenwell.jwell.mybatis.proxy.MyBatisProxy;
+
 public class MyBatisSupport {
+
+    private String configuration;
 
     @SuppressWarnings("unchecked")
     public static final <T> T craeteDao(Class<T> cls) {
@@ -12,6 +14,14 @@ public class MyBatisSupport {
         enhancer.setSuperclass(cls);
         enhancer.setCallback(new MyBatisProxy());
         return (T) enhancer.create();
+    }
+
+    public void init() {
+        System.out.println(configuration);
+    }
+
+    public void setConfiguration(String configuration) {
+        this.configuration = configuration;
     }
 
 }
