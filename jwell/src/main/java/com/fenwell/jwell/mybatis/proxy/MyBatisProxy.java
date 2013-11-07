abe7c8c3d65f260a02c68426a2bcfe87110da2d4
wang22 <pause22@163.com>
2013-11-07 12:48:03 +0800

    先提交吧。。 今天没什么心情写了

--- jwell/src/main/java/com/fenwell/jwell/mybatis/proxy/MyBatisProxy.java
+++ jwell/src/main/java/com/fenwell/jwell/mybatis/proxy/MyBatisProxy.java
@@ -2,19 +2,24 @@ package com.fenwell.jwell.mybatis.proxy;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Map;
+
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
 
 import com.fenwell.jwell.mybatis.annotation.Delete;
 import com.fenwell.jwell.mybatis.annotation.Insert;
+import com.fenwell.jwell.mybatis.annotation.Namespace;
 import com.fenwell.jwell.mybatis.annotation.SQL;
 import com.fenwell.jwell.mybatis.annotation.SelectList;
 import com.fenwell.jwell.mybatis.annotation.SelectOne;
 import com.fenwell.jwell.mybatis.annotation.Update;
-
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
+import com.fenwell.jwell.mybatis.transaction.Tran;
+import com.fenwell.util.Strings;
 
 public class MyBatisProxy implements MethodInterceptor {
 
@@ -28,8 +33,11 @@ public class MyBatisProxy implements MethodInterceptor {
             return null;
         }
         Object result = null;
+        Object param = makeParam();
+        String id = getId(an, target, mtd);
         if (an instanceof Insert) {
-            System.out.println("插入");
+            result = doInsert(id, mtd, param);
+            System.out.println("增加");
         } else if (an instanceof Delete) {
             System.out.println("删除");
         } else if (an instanceof Update) {
@@ -39,6 +47,56 @@ public class MyBatisProxy implements MethodInterceptor {
         } else if (an instanceof SelectList) {
             System.out.println("查询列表！");
         }
+        return result;
+    }
+
+    private String getId(Annotation an, Object target, Method mtd) {
+        String id = makeId(an, target, mtd);
+        return id;
+    }
+
+    private String makeId(Annotation an, Object target, Method mtd) {
+        Class<?> cls = target.getClass().getInterfaces()[0];
+        Namespace namespace = cls.getAnnotation(Namespace.class);
+        String ns = Strings.EMPTY;
+        if (namespace != null) {
+            if (Strings.isBlank(namespace.id())) {
+                ns = cls.getName();
+            } else {
+                ns = namespace.id();
+            }
+        }
+        String id = Strings.EMPTY;
+        if (an instanceof Insert) {
+            id = ((Insert) an).id();
+        } else if (an instanceof Delete) {
+            id = ((Delete) an).id();
+        } else if (an instanceof Update) {
+            id = ((Update) an).id();
+        } else if (an instanceof SelectOne) {
+            id = ((SelectOne) an).id();
+        } else if (an instanceof SelectList) {
+            id = ((SelectList) an).id();
+        }
+        if (Strings.isBlank(id)) {
+            id = mtd.getName();
+        }
+        return Strings.isEmpty(ns) ? id : ns + "." + id;
+    }
+
+    private Map<String, Object> makeParam() {
+        return null;
+    }
+
+    private boolean doInsert(String id, Method mtd, Object param) {
+        SqlSession session = Tran.getSession();
+        boolean result = false;
+        if (param == null) {
+            result = session.insert(id) > 0;
+        } else {
+            result = session.insert(id, param) > 0;
+        }
+        Tran.closeSession(session);
         return result;
     }
 
