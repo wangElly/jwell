16f64c9d678258b8050d5b978fc8bb7e37834fdf
wang22 <pause22@163.com>
2013-11-12 12:39:34 +0800

    拆分操作方法，降低耦合度。

--- jwell/src/main/java/com/fenwell/jwell/mybatis/operation/AbstractOperation.java
+++ jwell/src/main/java/com/fenwell/jwell/mybatis/operation/AbstractOperation.java
@@ -1,5 +1,9 @@
 package com.fenwell.jwell.mybatis.operation;
 
+import java.lang.reflect.Method;
+
 public abstract class AbstractOperation {
+
+    public abstract Object execute(String id, Method mtd, Object param);
 
 }
--- jwell/src/main/java/com/fenwell/jwell/mybatis/operation/DeleteOperation.java
+++ jwell/src/main/java/com/fenwell/jwell/mybatis/operation/DeleteOperation.java
@@ -1,5 +1,12 @@
 package com.fenwell.jwell.mybatis.operation;
 
+import java.lang.reflect.Method;
+
 public class DeleteOperation extends AbstractOperation{
+
+    @Override
+    public Object execute(String id, Method mtd, Object param) {
+        return null;
+    }
 
 }
